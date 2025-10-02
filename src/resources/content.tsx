f3f1ca9f7bf274b815327266943445c9caba78ac
bolaabanjo <royadebanjo@gmail.com>
2025-10-02 02:49:38 +0100

    updated portfolio

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -65,7 +65,7 @@ const home: Home = {
   },
   subline: (
     <>
-      
+      I'm Bola, but everyone I know calls me Roy. I'm a Mechanical Software Engineer, Design Engineer, AI Engineer.
     </>
   ),
 };
