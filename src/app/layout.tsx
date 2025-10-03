41333ec4860013374b8e7c09ec5c50eae953b548
bolaabanjo <royadebanjo@gmail.com>
2025-10-03 16:24:54 +0100

    updated portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@once-ui-system/core/css/styles.css";
 import "@once-ui-system/core/css/tokens.css";
 import "@/resources/custom.css";
+import { Analytics } from "@vercel/analytics/next"
 
 import classNames from "classnames";
 import {
@@ -158,6 +159,7 @@ export default async function RootLayout({
             <Footer />
           </Column>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
