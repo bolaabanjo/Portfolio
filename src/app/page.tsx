5c7b8d89d3274ab3f94a015445b3872ca3e9fab4
bolaabanjo <royadebanjo@gmail.com>
2025-11-15 18:10:34 +0100

    added threads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
                 Latest from the blog
               </Heading>
             </Row>
-            <Row flex={3} paddingX="20">
+            <Row flex={3} padding="20">
               <Posts range={[1, 2]} columns="2" />
             </Row>
           </Row>
