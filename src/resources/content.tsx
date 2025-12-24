c7f9cc381c23b7169f98d07d633b908052745e95
Bola Banjo <omogbolahanng@gmail.com>
2025-12-23 17:23:31 -0800

    fix: rename Gitstack featured work to Cencori and update its link

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -59,14 +59,14 @@ const home: Home = {
     display: true,
     title: (
       <Row gap="12" vertical="center">
-        <strong className="ml-4">Gitstack</strong>{" "}
+        <strong className="ml-4">Cencori</strong>{" "}
         <Line background="brand-alpha-strong" vert height="20" />
         <Text marginRight="4" onBackground="brand-medium">
           Featured work
         </Text>
       </Row>
     ),
-    href: "/work/building-gitstack",
+    href: "/work/cencori",
   },
   subline: (
     <>
