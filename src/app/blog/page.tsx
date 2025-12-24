ef715cf7d2eaa89af95bb8504cc3678b2d0769ad
Bola Banjo <omogbolahanng@gmail.com>
2025-12-23 16:32:04 -0800

    refactor: remove Mailchimp component and its imports from blog and home pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,6 @@
 import { Column, Heading, Meta, Schema } from "@once-ui-system/core";
-import { Mailchimp } from "@/components";
 import { Posts } from "@/components/blog/Posts";
-import { baseURL, blog, person, newsletter } from "@/resources";
+import { baseURL, blog, person } from "@/resources";
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -35,7 +34,6 @@ export default function Blog() {
       <Column fillWidth>
         <Posts />
       </Column>
-      <Mailchimp />
     </Column>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ import {
   Line,
 } from "@once-ui-system/core";
 import { home, about, person, baseURL, routes } from "@/resources";
-import { Mailchimp } from "@/components";
 import { Projects } from "@/components/work/Projects";
 import { Posts } from "@/components/blog/Posts";
 
@@ -123,7 +122,6 @@ export default function Home() {
           </Row>
         </Column>
       )}
-      <Mailchimp />
     </Column>
   );
 }
